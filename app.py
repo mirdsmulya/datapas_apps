f3bb6994b41f5f0ca1957bd5c078146c831ac52c
mirdsm <by_ms19@yahoo.com>
2018-05-08 17:37:57 +0700

    adding jquery 2

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ def home():
 
 @app.route('/signup')
 def signup():
-	return render_template('signup.html')
+	return render_template('test.html')
 
 
 @app.route('/signUpreq',methods=['POST'])
 def signUpreq():
- 
+
     _username = request.form['inputUsername']
     _name = request.form['inputName']
     _password = request.form['inputPassword']	
@@ -30,6 +30,24 @@ def signUpreq():
         return json.dumps({'html':'<span>Enter the required fields</span>'})
 
 
+'''
+
+
+
+@app.route('/signUp',methods=['POST'])
+def signUp():
+ 
+    # read the posted values from the UI
+    _name = request.form['inputName']
+    _email = request.form['inputEmail']
+    _password = request.form['inputPassword']
+ 
+    # validate the received values
+    if _name and _email and _password:
+        return json.dumps({'html':'<span>All fields good !!</span>'})
+    else:
+        return json.dumps({'html':'<span>Enter the required fields</span>'})
+'''
 
 if __name__ == '__main__':
 	app.run()
